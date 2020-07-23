1968b08b270108e7480faf15176843db4bed4f82
igoragueme <you@example.com>
2020-07-23 09:25:00 -0300

    Mudando alguns parametros

---
+++ demo/src/main/java/com/observability/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.observability.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> hello(){
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    }
+
+}
